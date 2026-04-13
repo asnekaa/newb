de5c27ab36a4845b2c9b9235d7f0a2b540370b51
ASneka <esnakesnake@gmail.com>
2026-04-13 16:12:34 +0800

    改了闪电的位置和缩放

--- content/modules/user-info-hover.js
+++ content/modules/user-info-hover.js
@@ -104,7 +104,7 @@ class UserInfoHoverEngine {
                 let verifyHtml = '';
         if (info.official && info.official.role !== 0) {
             const vColor = info.official.type === 0 ? '#f3a034' : '#00aeec';
-            verifyHtml = `<div class="newb-u-verify" style="color: ${vColor}"><svg width="16" height="16" viewBox="0 0 16 16"><circle cx="8" cy="8" r="8" fill="currentColor"/><g transform="translate(2.75, 2.75) scale(0.75)"><path d="M10.5 6h-3l1-4c.1-.3-.2-.5-.5-.4L3.5 7.5c-.2.2-.1.5.2.5h3l-1 4c-.1.3.2.5.5.4l4.5-5.9c.2-.2.1-.5-.2-.5z" fill="#fff"/></g></svg>${info.official.title}</div>`;
+            verifyHtml = `<div class="newb-u-verify" style="color: ${vColor}"><svg width="16" height="16" viewBox="0 0 16 16"><circle cx="8" cy="8" r="8" fill="currentColor"/><g transform="translate(1.65, 1.65) scale(0.9)"><path d="M10.5 6h-3l1-4c.1-.3-.2-.5-.5-.4L3.5 7.5c-.2.2-.1.5.2.5h3l-1 4c-.1.3.2.5.5.4l4.5-5.9c.2-.2.1-.5-.2-.5z" fill="#fff"/></g></svg>${info.official.title}</div>`;
         }
 
         const cookieMatch = document.cookie.match(/DedeUserID=([^;]+)/);
